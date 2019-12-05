59c76d412423a0604ffa1e25bda56594bcdbedf3
Rori Ash <aash@lcs.on.ca>
2019-12-05 14:55:17 -0500

    Created main storyboard and and connected labels and text fields.

--- MultipleChoice/ViewController.swift
+++ MultipleChoice/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Mark: Properties
+    
+    //UITextFields
+    @IBOutlet weak var NumberOfQuestions: UITextField!
+    @IBOutlet weak var StudentAnswers: UITextField!
+    @IBOutlet weak var CorrectAnswers: UITextField!
+    //Labels
+    @IBOutlet weak var NumberOfQuestionsLabel: UILabel!
+    @IBOutlet weak var StudentAnswersLabel: UILabel!
+    @IBOutlet weak var CorrectAnswersLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    // Check Answers Button
+    @IBAction func CheckAnswers(_ sender: Any) {
+        
+        guard let user = NumberOfQuestions.text
+            else { NumberOfQuestionsLabel.text = "Please enter an integer greated than 0"
+                return
+        }
+                guard let student = StudentAnswers.text
+                    else {
+                        StudentAnswersLabel.text = "Please be sure to enter"
+                    return
+                }
+        }
+    }
+    
 
-
-}
 
