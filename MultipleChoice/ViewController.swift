6e5668b26815481dc5544454e81390abc2483527
Rori Ash <aash@lcs.on.ca>
2019-12-09 15:28:38 -0500

    Working to iterate over each character

--- MultipleChoice/ViewController.swift
+++ MultipleChoice/ViewController.swift
@@ -28,25 +28,39 @@ class ViewController: UIViewController {
     // Check Answers Button
     @IBAction func CheckAnswers(_ sender: Any) {
         
-        
+        // Making number of questions into an integer
         let NumberOfQuestionsAsInt = Int (NumberOfQuestions.text!)
         
+        // Integer greater than 0
         guard let user = NumberOfQuestions
             else { NumberOfQuestionsLabel.text = "Please enter an integer greater than 0"
                 return
         }
+        // Number of student answers = number of questions
         guard let student = StudentAnswers.text, student.count == NumberOfQuestionsAsInt
             else {
                 StudentAnswersLabel.text = "The number of student answers must be the same as the number of questions."
                 return
         }
+        // Number of correct answers = number of questions
         guard let correct = CorrectAnswers.text, correct.count == NumberOfQuestionsAsInt
             else {
                 CorrectAnswersLabel.text = "The number of correct answers must be the same as the number of questions and student answers."
                 return
         }
+        
+        // Loop to print out each character
+        for (position, singleCharacter) in student.enumerated () {
+            print(singleCharacter)
+        }
+        for (position, singleCharacter) in correct.enumerated () {
+            print(singleCharacter)
+            
+        }
+        
+        
+        
     }
-    
-    
-    
 }
+
+
